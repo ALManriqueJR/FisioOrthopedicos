c07329e584cac0bd19f4d8d4bcac4c412296f1b8
AL Manrique JR <alessandromanriquejunior@gmail.com>
2022-09-09 17:33:02 -0300

    Heroku Deploy - Site v.1.5

---
+++ Pages/servicos.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+<html lang="pt-bt">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FisioOrthopédicos - Serviços</title>
+
+    <link rel="stylesheet" href="../CSS/style.css">
+    <link rel="icon" href="../img/Logo-icon.png" type="image/x-icon">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+
+<body>
+
+    <header>
+        <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid">
+                <a class="navbar-brand mx-auto" href="../index.php">
+                    <img src="../IMG/FisioOrtho.png" alt="FisioOrtho" width="100%" height="350%">
+                </a>
+        </nav>
+    </header>
+
+    <br>
+
+    <section class="container-fluid text-center margin">
+        <div class="container-fluid text-center margin">
+            <br><br>
+            <p style="font-weight: 600; font-size: 26px;">Agende uma consulta conosco. <br>Ligue para (15) 99147-6480.
+                <br> Temos WhatsApp!
+            </p>
+            <br>
+        </div>
+    </section>
+    <br>
+
+    <div class="container">
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+
+
+            <div class="carousel-inner">
+
+                <div class="item active">
+                    <img src="../IMG/consultorio/home.png" alt="Imagem Inical" style="width:100%;">
+                    <div class="carousel-caption">
+                        <h3 class="carousel-subt">Bem vindo!</h3>
+                        <p class="carousel-desc">FisiOrthopédicos agradece sua visita.</p>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <img src="../IMG/consultorio/consultorio.png" alt="Loja" style="width:100%;">
+                    <div class="carousel-caption">
+                        <h3 class="carousel-subt">Conheça nossos Serviços e Instalações.</h3>
+                        <p class="carousel-desc">Ambiente e instrumentação apropriados para cada necessidade.</p>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <img src="../IMG/servico.png" alt="Instalações" style="width:100%;">
+                    <div class="carousel-caption">
+                        <h3 class="carousel-subt">Nos faça uma visita!</h3>
+                        <p class="carousel-desc">Especialidade e exclusividade no atendimento.</p>
+                    </div>
+                </div>
+
+            </div>
+
+            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+
+    <section id="servicos">
+        <div class="container-fluid text-center margin">
+            <br>
+            <h3 style="font-size: 40px;">Serviços</h3>
+            <br>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <ul>
+                            <li>
+                                Medicina Chinesa
+                                <hr>
+                            </li>
+                            <li>
+                                Acupuntura Sistêmica
+                            </li>
+                            <li>
+                                Auriculoterapia Chinesa e Francesa
+                            </li>
+                            <li>
+                                Ventosaterapia
+                            </li>
+                        </ul>
+                        <br><br>
+                        <img class="serv-img" src="../IMG/acupuntura.jpg" alt="agulhas-paciente" width="100%"
+                            height="66%">
+                    </div>
+                    <div class="col-lg-4">
+                        <ul class="toomuchtxt">
+                            <li>
+                                Fisioterapia Ortopédica
+                                <hr>
+                            </li>
+                            <li>
+                                Pós operatório Ortopédica
+                            </li>
+                            <li>
+                                Lesões músculo-esquelética
+                            </li>
+                            <li>
+                                RPG
+                            </li>
+                        </ul>
+                        <img class="serv-img" src="../IMG/fisio.png" alt="tratamento-fisio" width="100%" height="66%">
+                    </div>
+                    <div class="col-lg-4">
+                        <ul>
+                            <li>
+                                Fisioterapia Facial
+                                <hr>
+                            </li>
+                            <li>
+                                Reabilitação Ortognática
+                            </li>
+                            <li>
+                                Disfunção de ATM
+                            </li>
+                            <li>
+                                Paralisia de Bell
+                            </li>
+                        </ul>
+                        <br><br>
+                        <img class="serv-img" src="../IMG/serv-3.png" alt="locação e venda" width="100%" height="66%">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer id="footer" class="container-fluid text-center margin bg-footer" style="margin-top: 4%;">
+        <div class="container-fluid text-center margin">
+            <p style="font-style: italic; font-weight: bold;">※Estacionamento exclusivo para clientes※</p>
+            <br>
+            <div class="row">
+                <div class="col-lg-4">
+                    <h4>Horário de Funcionamento</h4>
+                    <br>
+                    <p>
+                    <table>
+                        <tr>
+                            <th>Dias</th>
+                            <th></th>
+                            <th>⠀⠀⠀⠀Horários</th>
+                        </tr>
+                        <tr>
+                            <td><small>⠀⠀⠀⠀⠀</small></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td><small>Seg. a Sex.</small></td>
+                            <td></td>
+                            <td>⠀⠀⠀⠀⠀8~12h-14~18h</td>
+                        </tr>
+                        <tr>
+                            <td><small></small></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td><small></small></td>
+                            <td></td>
+                            <td>⠀⠀⠀⠀⠀</td>
+                        </tr>
+                        <tr>
+                            <td><small>Sáb.</small></td>
+                            <td></td>
+                            <td>⠀⠀⠀⠀8h-12h</td>
+                        </tr>
+                    </table>
+                    <br>
+                </div>
+
+                <div class="col-lg-4">
+                    <h4>Contato</h4>
+                    <br>
+                    <p>
+                        (15) 99147-6480
+                    </p>
+                    <br>
+                    <h4>Endereço</h4>
+                    <br>
+                    <a class="suave-hyperlink" target="_blank"
+                        href="https://www.google.com/maps/place/Av.+Pereira+da+Silva,+1953+-+Jardim+Santa+Ros%C3%A1lia,+Sorocaba+-+SP,+18095-340/@-23.4829528,-47.4393811,3a,75y,315.94h,89.51t/data=!3m6!1e1!3m4!1sfq0S1H-DquDUErLDJFShFQ!2e0!7i16384!8i8192!4m5!3m4!1s0x94cf6005fc2b59cd:0xaf1a9d3ea1b85180!8m2!3d-23.4828348!4d-47.4395607">Av.
+                        Pereira da Silva, 1953 - Jd. Sta. Rosália - Sala 5<br> Sorocaba - São Paulo</a>
+                </div>
+                <div class="col-lg-4">
+                    <br>
+                    <a href="https://www.facebook.com/fisioorthopedicos" class="fa fa-facebook" target="_blank"></a>
+                    <a href="https://www.instagram.com/fisioorthopedicos/" class="fa fa-instagram" target="_blank"></a>
+                    <a href="https://www.linkedin.com/in/dra-j%C3%A9ssica-arantes-a86666153/" class="fa fa-linkedin"
+                        target="_blank"></a>
+                    <br><br><br>
+                    <p>Copyright <a class="suave-hyperlink" href="https://github.com/ALManriqueJR" target="_blank">𝒜𝓁
+                            𝒥𝓇</a> - FisioOrthopédicos 2022 ™</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <script src="../bootstrap/jquery-3.6.0.min.js"></script>
+    <script src="../bootstrap/bootstrap.min.js"></script>
+    <script src="../bootstrap/js/prod.js" defer></script>
+
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,326 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <title>FisioOrthopédicos</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description"
+        content="Fisioterapia ortopédica e facial, tratamento para disfunção de ATM e Ortognática: RPG, Acupuntura, Ventosaterapia. Também realizando venda e locação de equipamentos.">
+    <meta name="author" content="AL Manrique Jr & README.md">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="CSS/style.css">
+    <link rel="stylesheet" href="CSS/prod.css">
+    <link rel="icon" href="IMG/Logo-icon.png" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+
+<body>
+
+    <header>
+        <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid">
+                <a class="navbar-brand mx-auto">
+                    <img src="IMG/Logo.png" alt="FisiOrthopédicos" width="100%" height="90">
+                </a>
+                <div class="collapse navbar-collapse">
+                    <ul class="navbar-nav nav-fill w-100">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#quem-somos">Quem somos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#prod-fisio">Produtos de Fisioterapia</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="Pages/servicos.php">Serviços</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#footer">Localização e Contatos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link">Login</a>
+                        </li>
+                    </ul>
+                </div>
+        </nav>
+
+    </header>
+
+    <div class="container">
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+
+            <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+            </ol>
+
+            <div class="carousel-inner">
+
+                <div class="item active">
+                    <img src="IMG/consultorio/home.png" alt="Imagem Inical" style="width:100%;">
+                    <div class="carousel-caption">
+                        <h3 class="carousel-subt">Bem vindo!</h3>
+                        <p class="carousel-desc">FisiOrthopédicos agradece sua visita.</p>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <img src="IMG/consultorio/conheca-loja.png" alt="Loja" style="width:100%;">
+                    <div class="carousel-caption">
+                        <h3 class="carousel-subt">Conheça nossa Loja.</h3>
+                        <p class="carousel-desc">Equipamentos e treinamento? Nós ajudamos você!</p>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <img src="IMG/consultorio/frente.png" alt="Instalações" style="width:100%;">
+                    <div class="carousel-caption">
+                        <h3 class="carousel-subt">Faça uma visita e agende instrução!</h3>
+                        <p class="carousel-desc">Ambiente acolhedor e infraestrutura excepcional.</p>
+                    </div>
+                </div>
+
+            </div>
+
+            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+
+    <section id="quem-somos">
+        <div class="container quem-somos">
+            <p class="bio">⠀⠀⠀⠀A FisioOrthopédicos foi criada com o intuito de proporcionar ao cliente excelência no
+                atendimento dentro e fora da loja; Dentro da loja com orientações sobre qual o melhor produto para cada
+                caso. Como usar e qual a altura ideal na hora da locação ou compra de bengala, andador ou muletas. Pois
+                é muito comum nos demais lugares o cliente locar e não é ensinado o mesmo a usar o produto de forma
+                correta, trazendo consequências no dia a dia, prejudicando sua saúde.</p>
+            <div class="row">
+                <div class="col-lg-6">
+                    <img class="bio-img" src="IMG/je.jpeg" alt="Nossa Equipe de Profissionais" width="68%">
+                </div>
+                <div class="col-lg-6 bio">
+                    <p>
+                        ⠀⠀⠀⠀E quando falo fora da loja, porque aqui o cliente encontra produtos para manter a prática de
+                        atividade física, também temos travesseiros que auxiliam numa boa qualidade de sono, e o mais
+                        importante com a qualidade que a coluna precisa para evitar lesões.
+                        O atendimento é realizado com profissionais treinados e também temos as orientações da Dra.
+                        Jéssica Arantes fisioterapeuta renomada aqui na cidade de Sorocaba, que trabalha com a área
+                        Ortopédica desde 2013.
+                        O consultório de fisioterapia FisioOrtho encontra-se no mesmo prédio que a loja.
+                    </p>
+                    <br>
+                    <p style="font-weight: 550; font-style: italic; text-align: center;"> "Venha cuidar da sua saúde,
+                        atendimento exclusivo com o diferencial de uma visão profissional".</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <div class="cont">
+
+        <h3 class="title" id="prod-fisio"> Produtos de Fisioterapia </h3>
+
+        <div class="products-container">
+
+            <div class="product" data-name="p-1">
+                <img src="IMG/muleta.png" alt="">
+                <h3>PROD</h3>
+                <div class="price">R$100,00</div>
+            </div>
+
+            <div class="product" data-name="p-2">
+                <img src="IMG/muleta.png" alt="">
+                <h3>PROD</h3>
+                <div class="price">R$100,00</div>
+            </div>
+
+            <div class="product" data-name="p-3">
+                <img src="IMG/muleta.png" alt="">
+                <h3>PROD</h3>
+                <div class="price">R$100,00</div>
+            </div>
+
+            <div class="product" data-name="p-4">
+                <img src="IMG/muleta.png" alt="">
+                <h3>PROD</h3>
+                <div class="price">R$100,00</div>
+            </div>
+
+            <div class="product" data-name="p-5">
+                <img src="IMG/muleta.png" alt="">
+                <h3>PROD</h3>
+                <div class="price">R$100,00</div>
+            </div>
+
+            <div class="product" data-name="p-6">
+                <img src="IMG/muleta.png" alt="">
+                <h3>PROD</h3>
+                <div class="price">R$100,00</div>
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="products-preview">
+
+        <div class="preview" data-target="p-1">
+            <i class="fas fa-times"></i>
+            <img src="IMG/muleta.png" alt="">
+            <h3>prod-1</h3>
+            <div>
+                <p>Venda; Locação; Aparelho Suporte;</p>
+
+            </div>
+            <p>As muletas são objetos utilizados como apoio para o corpo humano projetado com o propósito de auxiliar um
+                deficiente a caminhar quando uma das extremidades</p>
+            <div class="price">R$100,00</div>
+        </div>
+
+        <div class="preview" data-target="p-2">
+            <i class="fas fa-times"></i>
+            <img src="IMG/muleta.png" alt="">
+            <h3>prod-1</h3>
+            <div>
+                <p>Venda; Locação; Aparelho Suporte;</p>
+            </div>
+            <p>CARACTERISTICAS DESCRITIVAS DO PROD</p>
+            <div class="price">R$100,00</div>
+        </div>
+
+        <div class="preview" data-target="p-3">
+            <i class="fas fa-times"></i>
+            <img src="IMG/muleta.png" alt="">
+            <h3>muleta + descricao refinada</h3>
+            <div>
+                <p>Venda; Locação; Aparelho Suporte;</p>
+            </div>
+            <p>CARACTERISTICAS DESCRITIVAS DO PROD</p>
+            <div class="price">R$100,00</div>
+        </div>
+
+        <div class="preview" data-target="p-4">
+            <i class="fas fa-times"></i>
+            <img src="IMG/muleta.png" alt="">
+            <h3>muleta + descricao refinada</h3>
+            <div>
+                <p>Venda; Locação; Aparelho Suporte;</p>
+            </div>
+            <p>CARACTERISTICAS DESCRITIVAS DO PROD</p>
+            <div class="price">R$100,00</div>
+        </div>
+
+        <div class="preview" data-target="p-5">
+            <i class="fas fa-times"></i>
+            <img src="IMG/muleta.png" alt="">
+            <h3>muleta + descricao refinada</h3>
+            <div>
+                <p>Venda; Locação; Aparelho Suporte;</p>
+            </div>
+            <p>CARACTERISTICAS DESCRITIVAS DO PROD</p>
+            <div class="price">R$100,00</div>
+        </div>
+
+        <div class="preview" data-target="p-6">
+            <i class="fas fa-times"></i>
+            <img src="IMG/muleta.png" alt="">
+            <h3>muleta + descricao refinada</h3>
+            <div>
+                <p>Venda; Locação; Aparelho Suporte;</p>
+            </div>
+            <p>CARACTERISTICAS DESCRITIVAS DO PROD</p>
+            <div class="price">R$100,00</div>
+        </div>
+
+    </div>
+
+
+    <footer id="footer" class="container-fluid text-center margin bg-footer">
+        <div class="container-fluid text-center margin">
+            <p style="font-style: italic; font-weight: bold;">※Estacionamento exclusivo para clientes※</p>
+            <br>
+            <div class="row">
+                <div class="col-lg-4">
+                    <h4>Horário de Funcionamento</h4>
+                    <br>
+                    <p>
+                    <table>
+                        <tr>
+                            <th>Dias</th>
+                            <th></th>
+                            <th>⠀⠀⠀⠀Horários</th>
+                        </tr>
+                        <tr>
+                            <td><small>⠀⠀⠀⠀⠀</small></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td><small>Seg. a Sex.</small></td>
+                            <td></td>
+                            <td>⠀⠀⠀⠀⠀8~12h-14~18h</td>
+                        </tr>
+                        <tr>
+                            <td><small></small></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td><small></small></td>
+                            <td></td>
+                            <td>⠀⠀⠀⠀⠀</td>
+                        </tr>
+                        <tr>
+                            <td><small>Sáb.</small></td>
+                            <td></td>
+                            <td>⠀⠀⠀⠀8h-12h</td>
+                        </tr>
+                    </table>
+                    <br>
+                </div>
+
+                <div class="col-lg-4">
+                    <h4>Contato</h4>
+                    <br>
+                    <p>
+                        (15) 99116-3035
+                    </p>
+                    <br>
+                    <h4>Endereço</h4>
+                    <br>
+                    <a class="suave-hyperlink" target="_blank"
+                        href="https://www.google.com/maps/place/Av.+Pereira+da+Silva,+1953+-+Jardim+Santa+Ros%C3%A1lia,+Sorocaba+-+SP,+18095-340/@-23.4829528,-47.4393811,3a,75y,315.94h,89.51t/data=!3m6!1e1!3m4!1sfq0S1H-DquDUErLDJFShFQ!2e0!7i16384!8i8192!4m5!3m4!1s0x94cf6005fc2b59cd:0xaf1a9d3ea1b85180!8m2!3d-23.4828348!4d-47.4395607">Av.
+                        Pereira da Silva, 1953 - Jd. Sta. Rosália - Sala 1<br> Sorocaba - São Paulo</a>
+                </div>
+                <div class="col-lg-4">
+                    <br>
+                    <a href="https://www.facebook.com/fisioorthopedicos" class="fa fa-facebook" target="_blank"></a>
+                    <a href="https://www.instagram.com/fisioorthopedicos/" class="fa fa-instagram" target="_blank"></a>
+                    <a href="https://www.linkedin.com/in/dra-j%C3%A9ssica-arantes-a86666153/" class="fa fa-linkedin"
+                        target="_blank"></a>
+                    <br><br><br>
+                    <p>Copyright <a class="suave-hyperlink" target="_blank" href="https://github.com/ALManriqueJR">𝒜𝓁
+                            𝒥𝓇</a> - FisioOrthopédicos 2022 ™</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <script src="bootstrap/jquery-3.6.0.min.js"></script>
+    <script src="bootstrap/bootstrap.min.js"></script>
+    <script src="bootstrap/prod.js" defer></script>
+
+</body>
+
+</html>
